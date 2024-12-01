0e1c07d5b8bd44d7f8670bff17b8d8fda06a1394
Peter Sanford <psanford@sanford.io>
2024-12-01 13:47:00 -0800

    Add i7ie family

--- main.go
+++ main.go
@@ -1015,6 +1015,13 @@ var instanceTypes = []InstanceTypeInfo{
 		Flags:  GravitonSuffix,
 	},
 	{
+		// https://aws.amazon.com/blogs/aws/now-available-storage-optimized-amazon-ec2-i7ie-instances/
+		Name:   "i7ie",
+		Year:   2024,
+		Prefix: SSDPrefix,
+		Flags:  IntelSuffix | ExtendedMemorySuffix,
+	},
+	{
 		// https://aws.amazon.com/blogs/aws/introducing-storage-optimized-amazon-ec2-i8g-instances-powered-by-aws-graviton4-processors-and-3rd-gen-aws-nitro-ssds/
 		Name:   "i8g",
 		Year:   2024,
